70f2078d9c8060e67d67860163c169238040371a
kim3z <kim.lehtinen@hotmail.com>
2020-02-01 18:28:04 +0200

    Created LoginLoadingPage

---
+++ LoginLoadingPage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { View, Text, ActivityIndicator, StyleSheet, ImageBackground } from 'react-native'
+import firebase from 'react-native-firebase'
+import { Spinner } from 'native-base'
+
+export default class LoginLoadingPage extends React.Component {
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            // let nextPage = user ? 'HomePage' : 'LoginPage'
+            // this.props.navigation.navigate(nextPage)
+        })
+    }
+  
+    render() {
+        return (
+            <View style={styles.container}>
+                <Spinner color='blue' />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
