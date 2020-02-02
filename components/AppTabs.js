dd2eda68b8719c52231e8c678cc8057eb1828c8a
kim3z <kim.lehtinen@hotmail.com>
2020-02-02 20:06:31 +0200

    AppTabs component with functioning footer tabs

---
+++ components/AppTabs.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Content, Footer, FooterTab, Button, Text, Icon } from 'native-base';
+export default class AppTabs extends Component {
+  state = { 
+    selectedTab: 'moods' 
+  }
+
+  currentTab() {
+    switch (this.state.selectedTab) {
+      case 'moods':
+        return (<Text>Moods</Text>);
+        break;
+      case 'newmood':
+        return (<Text>New mood</Text>);
+        break;
+        case 'stats':
+          return (<Text>Stats</Text>);
+          break;
+      default:
+    }
+  }
+
+  render() {
+    return (
+        <>
+          <Content padder>
+            {this.currentTab()}
+          </Content>
+          <Footer>
+            <FooterTab>
+              <Button 
+              active={this.state.selectedTab==='moods'}
+              onPress={() => this.setState({selectedTab: 'moods'})}
+              >
+                <Icon type="FontAwesome" name="smile-o" />
+                <Text>Moods</Text>
+              </Button>
+              <Button 
+              active={this.state.selectedTab==='newmood'}
+              onPress={() => this.setState({selectedTab: 'newmood'})}
+              >
+                <Icon type="FontAwesome" name="plus" />
+                <Text>New Mood</Text>
+              </Button>
+              <Button
+              active={this.state.selectedTab==='stats'}
+              onPress={() => this.setState({selectedTab: 'stats'})}
+              >
+              <Icon type="Entypo" name="bar-graph" />
+                <Text>Stats</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        </>
+    );
+  }
+}
