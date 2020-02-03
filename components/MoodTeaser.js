2337cc7c1eb3868b6d8d66aab61813b8df8eaa7e
kim3z <kim.lehtinen@hotmail.com>
2020-02-03 15:10:04 +0200

    Show date in mood teaser as well as overall status

--- components/MoodTeaser.js
+++ components/MoodTeaser.js
@@ -3,37 +3,55 @@ import { Image } from 'react-native'
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base'
 
 export default class MoodTeaser extends Component {
-  render() {
-    const mood = this.props.mood
 
-    return (
-          <Card style={{flex: 0}}>
-            <CardItem>
-              <Left>
-                <Thumbnail source={require('../assets/clouds.jpg')} />
-                <Body>
-                    <Text>NativeBase</Text>
-                    <Text note>{mood.notes}</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Image source={require('../assets/clouds.jpg')} style={{height: 200, width: 200, flex: 1}}/>
-                <Text>
-                  //Your text here
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent textStyle={{color: '#87838B'}}>
-                  <Icon name="logo-github" />
-                  <Text>1,926 stars</Text>
-                </Button>
-              </Left>
-            </CardItem>
-          </Card>
-    );
-  }
+    teaserEmoji(overallMood) {
+        switch (overallMood) {
+            case 1:
+                return (<Icon type="FontAwesome5" name="sad-cry" />)
+                break
+            case 2:
+                return (<Icon type="FontAwesome" name="frown-o" />)
+                break
+            case 3:
+                return (<Icon type="FontAwesome" name="meh-o" />)
+                break
+            case 4:
+                return (<Icon type="FontAwesome" name="smile-o" />)
+                break
+            case 5:
+                return ''
+                break
+            default:
+        }
+    }
+
+    isMoodToday(moodDate) {
+        return new Date() === new Date(moodDate.toDate())
+    }
+
+    render() {
+        const mood = this.props.mood
+
+        return (
+            <Card style={{flex: 0}}>
+                <CardItem>
+                <Left>
+                    {this.teaserEmoji(mood.overall_mood)}
+                    <Body>
+                        <Text>{new Date(mood.date.toDate()).toDateString()}</Text>
+                        {this.isMoodToday(mood.date) && <Text note>TODAY</Text>}
+                    </Body>
+                </Left>
+                </CardItem>
+                <CardItem>
+                <Left>
+                    <Button transparent textStyle={{color: '#87838B'}}>
+                    <Icon name="logo-github" />
+                    <Text>1,926 stars</Text>
+                    </Button>
+                </Left>
+                </CardItem>
+            </Card>
+        );
+    }
 }
